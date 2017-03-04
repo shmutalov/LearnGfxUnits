bdc9413c951732cdf8a17f97f5ff0f583e1990d0
shmutalov <shmutalov@gmail.com>
2017-03-04 12:17:21 +0500

    Runable version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate gfx;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+use gfx::Device;
+use gfx::traits::FactoryExt;
+
+pub type ColorFormat = gfx::format::Rgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
+gfx_defines!{
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    constant Transform {
+        transform: [[f32; 4];4] = "u_Transform",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        transform: gfx::ConstantBuffer<Transform> = "Transform",
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+}
+
+const TRIANGLE: [Vertex; 3] = [
+    Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
+    Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
+    Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] }
+];
+
+// Identity matrix
+const TRANSFORM: Transform = Transform {
+    transform: [[1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0]]
+    };
+
+const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
+
+pub fn main() {
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Triangle example".to_string())
+        .with_dimensions(800, 600)
+        .with_vsync();
+
+    let (window, mut device, mut factory, main_color, mut main_depth) =
+        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
+
+    let pso = factory.create_pipeline_simple(
+            include_bytes!("shader/myshader_150.glslv"),
+            include_bytes!("shader/myshader_150.glslf"),
+            pipe::new()
+        ).unwrap();
+
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
+    let transform_buffer = factory.create_constant_buffer(1);
+    
+    let mut data = pipe::Data {
+            vbuf: vertex_buffer,
+            transform: transform_buffer,
+            out: main_color,
+        };
+
+    encoder.update_buffer(&data.transform, &[TRANSFORM], 0);
+
+    'main: loop {
+        for event in window.poll_events() {
+            match event {
+                glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
+                glutin::Event::Closed => break 'main,
+                glutin::Event::Resized(_width, _height) => {
+                    gfx_window_glutin::update_views(&window, &mut data.out, &mut main_depth);
+                },
+                _ => {}
+            }
+        }
+
+        encoder.clear(&data.out, CLEAR_COLOR);
+        encoder.draw(&slice, &pso, &data);
+        encoder.flush(&mut device);
+        window.swap_buffers().unwrap();
+        device.cleanup();
+    }
 }
